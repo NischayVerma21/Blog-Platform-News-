a3f97516971d8409ff670a69c71cc768a535f3c8
NischayVerma21 <nischaynis1@gmail.com>
2024-11-01 19:25:59 +0530

    Update connector.js

--- connector.js
+++ connector.js
@@ -2,7 +2,7 @@ let mysql = require('mysql');
  var con=mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"Nischay@1234",
+    password:"Your-sql-Password",
     database:"blog"
  })
 
